0ae67653f9a574f1c1db27a9a3da8a8747d55284
goreleaserbot <goreleaser@carlosbecker.com>
2021-06-04 19:21:52 -0700

    Brew formula update for spr version v0.4.0

--- spr.rb
+++ spr.rb
@@ -5,25 +5,25 @@
 class Spr < Formula
   desc "Stacked Pull Requests on GitHub"
   homepage "https://github.com/ejoffe/spr"
-  version "0.3.1"
+  version "0.4.0"
   license "MIT"
   bottle :unneeded
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/ejoffe/spr/releases/download/v0.3.1/spr_macos_x86_64.tar.gz"
-    sha256 "4fbb760a5ff967b5217b1abb76e8b6732d376f34a4ddbf078b086ad041d36ec6"
+    url "https://github.com/ejoffe/spr/releases/download/v0.4.0/spr_macos_x86_64.tar.gz"
+    sha256 "2a397de060186897b97d330e4ad9fa65a49b63cb032ac445361b4b4bf50317f6"
   end
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/ejoffe/spr/releases/download/v0.3.1/spr_macos_arm64.tar.gz"
-    sha256 "ed8d0e0b77a1de70d7451d0c2ad850ea2d843330726359babdc70233ac0bfa71"
+    url "https://github.com/ejoffe/spr/releases/download/v0.4.0/spr_macos_arm64.tar.gz"
+    sha256 "cc69aab349b8b423d1e84ea5a9b3f81c6bca1b066be7d4aa6d5bc38fe74a7c6f"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/ejoffe/spr/releases/download/v0.3.1/spr_linux_x86_64.tar.gz"
-    sha256 "46628ce75891d750451bdd30d0702c4da2acceff4da0ab8d90b1578149280c9d"
+    url "https://github.com/ejoffe/spr/releases/download/v0.4.0/spr_linux_x86_64.tar.gz"
+    sha256 "6de04ab930a1548f5b7234721629ddb4756cf67d570637b0d8622db210f1fae4"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/ejoffe/spr/releases/download/v0.3.1/spr_linux_arm64.tar.gz"
-    sha256 "b5a1342c2cc7f875f4f9db083b53ace5c602bf8dbe50a2d6f0aadb346a414418"
+    url "https://github.com/ejoffe/spr/releases/download/v0.4.0/spr_linux_arm64.tar.gz"
+    sha256 "d58086e7ef2cb3cc7a04dc9015c01cece2b536a0c823c5f558ecfdfe926f3be4"
   end
 
   def install
